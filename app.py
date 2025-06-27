259d6297bcfd34841588a797bb5a39dcdf8e9be1
Joyel Porunnedath Biju <joyelpbiju@gmail.com>
2025-06-28 01:08:18 +0200

    add db for  data + indexpage  listing

--- app.py
+++ app.py
@@ -134,7 +134,6 @@ def delete():
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(UPLOAD_FOLDER, filename
-
+    return send_from_directory(UPLOAD_FOLDER, filename)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
